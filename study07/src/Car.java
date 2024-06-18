57537045c41170d2cfac93b3cd43468d7da84e89
ejshinn <ejshin.dev@gmail.com>
2024-06-18 16:47:18 +0900

    final 메서드, final 클래스

---
+++ study07/src/Car.java
@@ -0,0 +1,18 @@
+// 부모 클래스로 사용되는 Car 클래스
+public class Car {
+
+    // 필드
+    public int speed;
+
+    // 메서드
+    public void speedUp() {
+        speed += 1;
+    }
+
+    // final 메서드, 상속은 가능하지만 오버라이딩은 불가능
+    public final void stop() {
+        System.out.println("차를 멈춤");
+        speed = 0;
+    }
+
+}
